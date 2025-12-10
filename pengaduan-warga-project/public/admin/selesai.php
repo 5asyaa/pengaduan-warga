423b3415a0d72a5c9cf692be3dfc8ccb4f92fee7
5asyaa <aisyahaisyah1312@gmail.com>
2025-12-10 11:02:22 +0800

    Update selesai.php

--- pengaduan-warga-project/public/admin/selesai.php
+++ pengaduan-warga-project/public/admin/selesai.php
@@ -4,10 +4,10 @@ require_once "../../controllers/AdminController.php";
 
 $controller = new AdminController($pdo);
 
-// Jika tombol submit ditekan → proses selesai
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $controller->submitSelesai($_GET['id']);
     exit;
 }
 
 $controller->selesai($_GET['id']);
+
